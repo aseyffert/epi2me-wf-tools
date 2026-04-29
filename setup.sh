09453ef4060338c7a08514b718155384f77a3ffc
aseyffert <9977207+aseyffert@users.noreply.github.com>
2026-04-30 00:56:52 +0200

    Update planned responsibilities

--- setup.sh
+++ setup.sh
@@ -5,11 +5,7 @@
 #
 # Currently planned responsibilities:
 # For users:
-#  - Create ${HOME}/.singularity_cache/ (if it doesn't already exist)
-#  - Check that their group ID is correct, so they can access
-#      /mnt/lustre/groups/CBBI1691/
-#  - Patch .bashrc such that:
-#     - epi2me-wf-tools's tools are on PATH,
-#     - NXF_SINGULARITY_CACHEDIR is set to ${HOME}/.singularity_cache/
-#     - NXF_SINGULARITY_LIBRARYDIR is set to
-#         /mnt/lustre/groups/CBBI1691/singularity_images/
+#  - Create $NXF_HOME and $NXF_SINGULARITY_CACHEDIR
+#  - [optional] Patch .bashrc such that $EWT_HOME/bash.env is sourced at login.
+
+source $(dirname $(realpath $0))/bash.env
