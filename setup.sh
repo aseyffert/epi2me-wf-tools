5a7bbf12cbd25e430ce184212f9efe50ffb824f7
aseyffert <9977207+aseyffert@users.noreply.github.com>
2026-04-30 00:59:27 +0200

    Implement init step for nextflow-related directories

--- setup.sh
+++ setup.sh
@@ -8,4 +8,21 @@
 #  - Create $NXF_HOME and $NXF_SINGULARITY_CACHEDIR
 #  - [optional] Patch .bashrc such that $EWT_HOME/bash.env is sourced at login.
 
+if [[ $BASH_SOURCE != $0 ]]; then
+  echo >&2 "ERROR: $BASH_SOURCE was sourced; must be executed."
+  return 1
+fi
+
 source $(dirname $(realpath $0))/bash.env
+
+for var_name in NXF_HOME NXF_SINGULARITY_CACHEDIR; do
+  if ! [[ ${!var_name} == /* ]]; then
+    echo >&2 "ERROR: $0 expects an absolute path for ${var_name}."
+    exit 1
+  fi
+  if ! [[ ${!var_name} == $USER_LUSTRE/* ]]; then
+    echo >&2 "ERROR: Bad $var_name; must be in ${USER_LUSTRE}."
+    exit 2
+  fi
+done
+mkdir -p "$NXF_HOME" "$NXF_SINGULARITY_CACHEDIR"
