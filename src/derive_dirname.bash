c4e89618509da7b7913a6ac7108282518d7d3893
aseyffert <9977207+aseyffert@users.noreply.github.com>
2026-08-17 21:58:45 +0200

    Add a vebosity toggle to derive_dirname

--- src/derive_dirname.bash
+++ src/derive_dirname.bash
@@ -1,8 +1,18 @@
 derive_dirname() {
-  local full_path
+  local OPTIND OPTARG OPTERR opt verbose=1 full_path
+  # FIXME: This should likely use OPTERR to toggle verbosity (somehow)
+  while getopts 'q' opt; do
+    case $opt in
+    q) verbose=0 ;;
+    *) return 1 ;;
+    esac
+  done
+  shift $((OPTIND - 1))
   full_path="$(realpath "$1" 2>/dev/null)" || {
-    echo >&2 "ERROR: Unable to resolve realpath for $1."
-    return 1
+    ((verbose)) && {
+      echo >&2 "ERROR: Unable to resolve realpath for $1."
+    }
+    return 3
   }
   echo "${full_path%/*}"
 }
